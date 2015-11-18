a1b412e7d45c78f84c59558f88eba12f8c23f41e
Jonata Weber <jonataa@gmail.com>
2015-11-18 15:48:56 -0300

    working on the API REST

--- desafio3/public/index.php
+++ desafio3/public/index.php
@@ -2,16 +2,24 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
+use App\Todo\TaskBundle\Task;
 use App\Todo\TaskBundle\TaskRepository;
 
 $app = new \Slim\App;
 $rep = new TaskRepository(new PDO('sqlite:../db/database.sq3'));
 
-$app->get('/task', function ($request, $response, $args) use ($rep) {
-  var_dump(json_encode($rep->getAll())); die;
+$app->post('/task', function ($request, $response) use ($rep) {
+  $data = $request->getParsedBody();
+  if (empty($data['task']) || empty($data['done']))
+      return $response->withStatus(400)->write('Required field missing');
+  $task = $rep->save(new Task($data['task'], $data['done']));
+  return $response->withStatus(201)->write(json_encode($task));
+});
+
+$app->get('/task', function ($request, $response) use ($rep) {
     return $response
       ->withHeader('Content-type', 'application/json')
-      ->write([123]);
+      ->write(json_encode($rep->getAll()));
 });
 
 $app->run();
