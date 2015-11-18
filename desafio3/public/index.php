51a634eaaf95d87341a7e51f73d4d163979a924f
Jonata Weber <jonataa@gmail.com>
2015-11-18 17:21:02 -0300

    working on the API REST

--- desafio3/public/index.php
+++ desafio3/public/index.php
@@ -5,26 +5,15 @@ require_once __DIR__.'/../vendor/autoload.php';
 use App\Todo\TaskBundle\Task;
 use App\Todo\TaskBundle\TaskRepository;
 
-$app = new \Slim\App;
-$rep = new TaskRepository(new PDO('sqlite:../db/database.sq3'));
+$container = new \Slim\Container;
+$container['repository'] = new TaskRepository(new PDO('sqlite:../db/database.sq3'));
+$app = new \Slim\App($container);
 
-$app->post('/task/', function ($request, $response) use ($rep) {
-  $data = $request->getParsedBody();
-  if (empty($data['task']) || empty($data['done']))
-      return $response->withStatus(400)->write('Required field missing');
-  $task = $rep->save(new Task($data['task'], $data['done']));
-  return $response->withStatus(201)->write(json_encode($task));
-});
+# debug mode
+// $app->settings['displayErrorDetails'] = true;
 
-$app->get('/task/', function ($request, $response) use ($rep) {
-  return $response
-    ->withHeader('Content-type', 'application/json')
-    ->write(json_encode($rep->getAll()));
-});
-
-$app->delete('/task/{id}/', function ($request, $response, $args) use ($rep) {
-  $rep->removeById($args['id']);
-  return $response->withStatus(204);
-});
+$app->post('/task/', App\Todo\Rest\TaskCreate::class);
+$app->get('/task/', App\Todo\Rest\TaskList::class);
+$app->delete('/task/{id}/', App\Todo\Rest\TaskDelete::class);
 
 $app->run();
