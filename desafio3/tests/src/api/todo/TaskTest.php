fa8310529ff130cab489b501c68c1cf5a80c2e7a
Jonata Weber <jonataa@gmail.com>
2015-11-18 12:41:40 -0300

    Create unittests

--- desafio3/tests/src/api/todo/TaskTest.php
+++ desafio3/tests/src/api/todo/TaskTest.php
@@ -1,20 +1,14 @@
 <?php
 
+require 'tests/AbstractTestCase.php';
 require 'src/Api/Todo/TaskBundle/Task.php';
 require 'src/Api/Todo/TaskBundle/TaskRepository.php';
 
 use Api\Todo\TaskBundle\Task;
 use Api\Todo\TaskBundle\TaskRepository;
 
-class TaskTest extends PHPUnit_Framework_TestCase
+class TaskTest extends AbstractTestCase
 {
-
-  public function createPDO()
-  {
-    $pdo = new PDO('sqlite::memory:');
-    $pdo->exec("CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, done INTEGER NOT NULL DEFAULT 0)");
-    return $pdo;
-  }
 
   public function testAddNewTask()
   {
