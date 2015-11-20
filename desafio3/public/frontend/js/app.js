35e8047f3701d8568d478f81e8d958b570b9d619
Jonata Weber <jonataa@gmail.com>
2015-11-20 15:06:25 -0300

    working on the frontend

--- desafio3/public/frontend/js/app.js
+++ desafio3/public/frontend/js/app.js
@@ -1,7 +1,16 @@
 angular.module('myTodoList', [])
-  .controller('TodoListCtrl', function ($scope) {    
+  .controller('TodoListCtrl', function ($scope) {
     $scope.tasks = [
-      {'title': 'Fazer desafio da iTFLEX', done: false},
-      {'title': 'Tomar um café', done: true}
+      {title: 'Fazer desafio da iTFLEX', done: false},
+      {title: 'Tomar um café', done: true}
     ];
+
+    $scope.submit = function () {
+      $scope.tasks.push({'title': $scope.newTitle, 'done': false});
+      $scope.newTitle = '';
+    };
+
+    $scope.delete = function (key) {
+      $scope.tasks.splice(key, 1);
+    };
   });
